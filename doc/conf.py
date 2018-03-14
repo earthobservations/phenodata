2e89bad0d54b6a2bada9e478358d4ab3031356f7
Andreas Motl <andreas@hiveeyes.org>
2018-03-14 19:40:33 +0100

    Bump version: 0.6.1 → 0.6.2

--- doc/conf.py
+++ doc/conf.py
@@ -24,9 +24,9 @@ copyright = u'2018, Andreas Motl'
 author = u'Andreas Motl'
 
 # The short X.Y version
-version = u'0.6.1'
+version = u'0.6.2'
 # The full version, including alpha/beta/rc tags
-release = u'0.6.1'
+release = u'0.6.2'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requires = [
 ]
 
 setup(name='phenodata',
-    version='0.6.1',
+    version='0.6.2',
     description='phenodata is a data acquisition and manipulation toolkit for open access phenology data',
     long_description=README,
     license="AGPL 3",
