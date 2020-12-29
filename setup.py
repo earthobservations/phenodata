06828619f4a5e3d1e3f51023e3badd30635d9626
Andreas Motl <andreas@hiveeyes.org>
2020-12-29 06:33:43 +0100

    Bump version: 0.9.4 → 0.10.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requires = [
 ]
 
 setup(name='phenodata',
-    version='0.9.4',
+    version='0.10.0',
     description='phenodata is a data acquisition and manipulation toolkit for open access phenology data',
     long_description=README,
     license="AGPL 3",
