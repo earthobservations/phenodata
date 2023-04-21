e31865fd9ada33e58f9c763c39f74b2d874ff6fb
Andreas Motl <andreas.motl@panodata.org>
2023-04-21 22:06:13 +0200

    Documentation: Add logo image

--- doc/conf.py
+++ doc/conf.py
@@ -81,12 +81,15 @@ html_theme = "furo"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    "light_logo": "phenodata-logo-light.svg",
+    "dark_logo": "phenodata-logo-dark.svg",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
