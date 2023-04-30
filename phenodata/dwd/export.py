fad92a460d53fdbfa9a610f0fb3d8555e72ddf1f
Andreas Motl <andreas.motl@panodata.org>
2023-04-30 14:26:49 +0200

    SQLite export: Add `dwd_species_group` table from `presets.json` file

--- phenodata/dwd/export.py
+++ phenodata/dwd/export.py
@@ -1,6 +1,8 @@
 import logging
 import typing as t
 
+import pandas as pd
+
 from phenodata.dwd.model import DwdPhenoDataset, DwdPhenoDatabase, DwdPhenoPartition
 from phenodata.dwd.pheno import DwdPhenoDataClient
 
@@ -12,10 +14,15 @@ def acquire_database(client: DwdPhenoDataClient, options: t.Optional[t.Dict[str,
     options = options or {}
     dataset = options.get("dataset", "unknown")
     partition = options.get("partition", "unknown")
+
+    species = client.get_species()
+    species_group = get_species_presets_df(species)
+
     db = DwdPhenoDatabase(
         dataset=DwdPhenoDataset(dataset),
         partition=DwdPhenoPartition(partition),
-        species=client.get_species(),
+        species=species,
+        species_group=species_group,
         phase=client.get_phases(),
         quality_level=client.get_quality_levels(),
         quality_byte=client.get_quality_bytes(),
@@ -37,3 +44,25 @@ def export_database(client, target, options):
     # Optionally print samples.
     # print(db.observations)
     logger.info(f"Exported data to {target}")
+
+
+def get_species_presets_df(species: pd.DataFrame):
+    """
+    Convert species groups from `presets.json` into DataFrame.
+    """
+    data = DwdPhenoDataClient.load_preset_species()
+    outdata = []
+    for group_name, items_raw in data.items():
+        items = list(map(str.strip, items_raw.split(",")))
+        for item in items:
+            result = species.query("Objekt == @item")
+            try:
+                species_id = result.index.values[0]
+            except:
+                logger.warning(f"Unable to discover species name in data: {item}")
+                continue
+            outitem = {"species_id": species_id, "group_name": group_name, "species_name_de": item}
+            outdata.append(outitem)
+    outdf = pd.DataFrame.from_records(outdata, index="species_id")
+    outdf.attrs["name"] = "species_group"
+    return outdf
