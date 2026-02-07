cf3e45352749eee9116d2ad1d5de9ce9641e9868
Andreas Motl <andreas.motl@panodata.org>
2026-02-07 12:53:46 +0100

    Bump version: 0.13.1 → 0.14.0

--- phenodata/__init__.py
+++ phenodata/__init__.py
@@ -1,3 +1,3 @@
 """phenodata is an acquisition and processing toolkit for open access phenology data"""
 __appname__ = 'phenodata'
-__version__ = '0.13.1'
+__version__ = '0.14.0'
--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ test_requires = [
 ]
 
 setup(name='phenodata',
-    version='0.13.1',
+    version='0.14.0',
     description='phenodata is an acquisition and processing toolkit for open access phenology data',
     long_description=README,
     license="AGPL-3.0",
